d3c85c88b312865f506fb370d4166c83862b994a
Kyle Keen <keenerd@gmail.com>
2017-10-11 08:49:54 -0400

    failing testcase for unique

--- tests/_setup.sh
+++ tests/_setup.sh
@@ -3,3 +3,5 @@
 shuf /usr/share/dict/words | tr '[A-Z]' '[a-z]' | tr -dc '[a-z\n]' | gzip > tests/random_words.gz
 zcat tests/random_words.gz | sort | gzip > tests/sorted_words.gz
 
+echo -en '1\n2\n2\n3\n2\n' | gzip > tests/small.gz
+
