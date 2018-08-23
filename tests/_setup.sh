ec6693102a27580955554cfcdde66c0fed6e7598
Kyle Keen <keenerd@gmail.com>
2018-08-22 20:45:24 -0400

    fix tests with non-ascii input

--- tests/_setup.sh
+++ tests/_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sort -R /usr/share/dict/words | tr '[A-Z]' '[a-z]' | tr -dc '[a-z\n]' | gzip > tests/random_words.gz
-zcat tests/random_words.gz | sort | gzip > tests/sorted_words.gz
+zcat tests/random_words.gz | LANG=C sort | gzip > tests/sorted_words.gz
 
 echo -en '1\n2\n2\n3\n2\n' | gzip > tests/small.gz
 
